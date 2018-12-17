ab5d2453108e66e2757a1465bb7794e64c8c3669
Marc Missey <marcmissey@gmail.com>
2018-12-17 09:41:58 -0700

    Mcfe 1123: Badges on Accordion (#339)
    * feat: add badges to accordian title
    
    * chore: snaps
    
    * chore: image snapshots
    
    * chore: fix pie-chart issues

--- src/accordion/accordion-panel-title.tsx
+++ src/accordion/accordion-panel-title.tsx
@@ -1,18 +1,32 @@
 import React, { Fragment } from 'react';
-
+import Badge from '../badge';
+import cn from '../utilities/classnames';
+import Styles from './accordion.module.scss';
 export interface AccordionPanelTitleTextProps {
   text: string;
   style?: React.CSSProperties;
   className?: string;
+  badgeColor?: string;
+  badgeContent?: string | number;
 }
 const AccordionPanelTitle: React.SFC<AccordionPanelTitleTextProps> = ({
   text,
   style,
   className,
+  badgeColor,
+  badgeContent,
   ...attributes
 }) => (
-  <h2 className={className} style={style} {...attributes}>
+  <h2
+    className={cn(className, {
+      'has-badge': !!badgeContent,
+      [Styles['has-badge']]: !!badgeContent,
+    })}
+    style={style}
+    {...attributes}
+  >
     {text}
+    {badgeContent && <Badge color={badgeColor}>{badgeContent}</Badge>}
   </h2>
 );
 
