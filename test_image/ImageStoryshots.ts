d04b0aa2ff67d7711f6874a9868adce8b3a8bf38
Alex Thomsen <alex.scott.thomsen@gmail.com>
2019-01-25 10:54:33 -0800

    Maybe fix this?

--- test_image/ImageStoryshots.ts
+++ test_image/ImageStoryshots.ts
@@ -1,4 +1,5 @@
-import initStoryshots, { imageSnapshot } from '@storybook/addon-storyshots';
+import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 import path from 'path';
 import process from 'process';
 
